c00aaedcc9f4706b517c0311cfa9a4774ce30f23
Nithya-Princy <141929247+Nithya-Princy@users.noreply.github.com>
2023-11-12 21:53:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+var printFunctionName = function() {
+    console.log(arguments.callee.name);
+};
+
+printFunctionName(); // This will print "printFunctionName" to the console
+
